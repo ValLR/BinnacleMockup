7bf15a78b91eb36c9701b8fb72c45286c99c9deb
ValLR <valentinalopezrecabal@gmail.com>
2018-01-26 18:08:11 -0300

    adding react-beautiful-dnd

--- src/components/BinnacleTable.js
+++ src/components/BinnacleTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import pivotalData from '../data/pivmock.json';
 import StoryRow from './StoryRow'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 class BinnacleTable extends Component {
 
@@ -8,14 +9,14 @@ class BinnacleTable extends Component {
 		let points = 0;
 
 		return(
-			<div>
+			<div className="table">
 			    <div className="row head">
 			        <div className="col-lg-3 col-md-3 col-sm-3 text-center">Proyecto</div>
 			        <div className="col-lg-3 col-md-3 col-sm-3 text-center">Epic</div>
 			        <div className="col-lg-3 col-md-3 col-sm-3 text-center">Estado</div>
 			        <div className="col-lg-3 col-md-3 col-sm-3 text-center">Puntos</div>
 				</div>
-		      	<div>
+		      	<div className="body">
 					{pivotalData.data[0].projects.map((project) =>
 						project.stories.map((story) =>
 						<StoryRow  key={story.id} project={project.canonicalName} epic={story.epic} pivState={story.state} points={story.points} />
--- src/components/StoryRow.js
+++ src/components/StoryRow.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
 
-function dragstart_handler(ev) {
-	console.log("dragstart");
-	
-	ev.dataTransfer.setData("text/plain", ev.target.id);
- 	ev.dataTransfer.setData("text/html", "<p>Example paragraph</p>");
-  	ev.dataTransfer.setData("text/uri-list", "http://developer.mozilla.org");
-}
-
 class StoryRow extends Component {
 
 	constructor(props) {
@@ -33,7 +25,7 @@ class StoryRow extends Component {
 	render() {
 
 		return (
-			<div draggable="true" className="story row" onDragStart={dragstart_handler}>
+			<div draggable="true" className="story row">
 				<div className="col-lg-3 col-md-3 col-sm-3 text-center">{this.props.project}</div>
 				<div className="col-lg-3 col-md-3 col-sm-3 text-center">{this.props.epic}</div>
 				<div className="col-lg-3 col-md-3 col-sm-3 text-center">{this.props.pivState}</div>
